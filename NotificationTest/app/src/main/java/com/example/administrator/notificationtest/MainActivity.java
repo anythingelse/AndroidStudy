7ee2080e849b268ae529a37953d7a0582de8a7cb
Wang <949518713@qq.com>
2018-09-01 13:01:03 +0800

    改notification

--- NotificationTest/app/src/main/java/com/example/administrator/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/administrator/notificationtest/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.administrator.notificationtest;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,20 +14,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.nio.channels.Channel;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NOTIFICATION_CHANNEL_ID = "test.default";
+    private static final String NOTIFICATION_CHANNEL_NAME = "test.default";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= 26){
+            NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    NOTIFICATION_CHANNEL_NAME,NotificationManager.IMPORTANCE_LOW);
+            manager.createNotificationChannel(mChannel);
+        }
         Button button = findViewById(R.id.btn_send);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,NotificationActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,0);
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                Notification notification = new NotificationCompat.Builder(MainActivity.this)
+                Notification notification = new NotificationCompat.Builder(MainActivity.this.getApplicationContext())
                         .setContentTitle("This is content title")
                         .setContentText("This is content text")
                         .setWhen(System.currentTimeMillis())
@@ -33,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                         .setContentIntent(pendingIntent)
                         .setAutoCancel(true)
+                        .setChannelId(NOTIFICATION_CHANNEL_ID)
                         .build();
                 manager.notify(1,notification);
                 Toast.makeText(MainActivity.this,"111",Toast.LENGTH_SHORT).show();
